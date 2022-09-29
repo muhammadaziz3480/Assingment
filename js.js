87326d0dfad627a79224084276cf7c1e97b5f313
muhammadaziz3480 <112552480+muhammadaziz3480@users.noreply.github.com>
2022-09-29 02:45:47 -0700

    Javascript Assignment.js

---
+++ js.js
@@ -0,0 +1,28 @@
+// // const number = parseInt(prompt("Enter a number"));
+
+// // for(let i = 1; i <= 10; i++){
+
+// //     const result = i * number
+    
+// //     document.write("<br>" ,`${number} x ${i} = ${result}`);
+// // }
+
+// const operator = prompt("Enter operator (+ , -, * or /):");
+// const number1 = parseFloat(prompt('Enter First number'));
+// const number2 = parseFloat(prompt('Enter second number'));
+
+// let result;
+
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-'){
+//     result = number1 - number2;
+// }
+// else if (operator == '*'){
+//     result = number1 * number2;
+// }
+// else{
+//     result = number1 / number2
+// }
+// document.write(`${number1} ${number2} = ${result}`)
